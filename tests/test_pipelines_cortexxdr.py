6739e3a1b3a73395ce4fc72548bed51acf7c9c75
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2023-06-30 21:36:39 -0500

    remove unnecessary import

--- tests/test_pipelines_cortexxdr.py
+++ tests/test_pipelines_cortexxdr.py
@@ -1,5 +1,4 @@
 import pytest
 from sigma.backends.cortexxdr import CortexXDRBackend
-from sigma.pipelines.cortexxdr import # TODO: import pipeline functions
 
 # TODO: import tests for all implemented pipelines and contained transformations
