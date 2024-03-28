2db048a108ac5222023f5796c82839f8d9da7a1e
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2024-03-28 08:18:34 -0500

    fix unit tests

--- tests/test_pipelines_cortexxdr.py
+++ tests/test_pipelines_cortexxdr.py
@@ -83,6 +83,37 @@ def test_cortexxdr_integrity_levels_filter_multiple(cortexxdr_backend: CortexXDR
         """)
     ) == ['preset=xdr_process | filter (event_type = ENUM.PROCESS and event_sub_type = ENUM.PROCESS_START) and (agent_os_type = ENUM.AGENT_OS_WINDOWS and (((action_process_integrity_level gte 4096 and action_process_integrity_level lt 8192) or (action_process_integrity_level gte 12288 and action_process_integrity_level lt 16384))))']
 
+def test_cortexxdr_generic_translation_mapping(cortexxdr_backend: CortexXDRBackend):
+    assert cortexxdr_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: file_event
+                product: test_product
+            detection:
+                sel:
+                    User: admin
+                    CommandLine: cmdline /field=value,
+                    Image: cmd.exe,
+                    LogonId: logon_id_text,
+                    Product: Product Name,
+                    Company: Company Name
+                    IntegrityLevel: High,
+                    CurrentDirectory: /current/dir/value,
+                    ProcessId: 1,
+                    ParentProcessId: 2,
+                    ParentCommandLine: cmdline_parent /field1=value1,
+                    ParentImage: explorer.exe,
+                    ParentUser: guest,
+                    ParentIntegrityLevel: Medium,
+                    ParentLogonId: logon_id_text_1,
+                    ParentProduct: Parent Product Name,
+                    ParentCompany: Parent Company Name
+                condition: sel
+        """)
+    ) == ['preset=xdr_file | filter event_type = ENUM.FILE and (actor_effective_username = "admin" and actor_process_image_command_line = "cmdline /field=value," and actor_process_image_path = "cmd.exe," and actor_process_logon_id = "logon_id_text," and actor_process_signature_product = "Product Name," and actor_process_signature_vendor = "Company Name" and actor_process_integrity_level = "High," and actor_process_cwd = "/current/dir/value," and actor_process_os_id = "1," and causality_actor_process_os_id = "2," and causality_actor_process_command_line = "cmdline_parent /field1=value1," and causality_actor_process_image_path = "explorer.exe," and causality_actor_effective_username = "guest," and causality_actor_process_integrity_level = "Medium," and causality_actor_process_logon_id = "logon_id_text_1," and causality_actor_process_signature_product = "Parent Product Name," and causality_actor_process_signature_vendor = "Parent Company Name")']
+
 def test_cortexxdr_process_creation_mapping(cortexxdr_backend : CortexXDRBackend):
     assert cortexxdr_backend.convert(
         SigmaCollection.from_yaml("""
@@ -93,27 +124,28 @@ def test_cortexxdr_process_creation_mapping(cortexxdr_backend : CortexXDRBackend
                 product: test_product
             detection:
                 sel:
-                    ProcessId: 12
-                    Image: valueA
-                    Product: bar foo
-                    Company: foo foo
-                    CommandLine: invoke-mimikatz
-                    CurrentDirectory: /etc
-                    User: administrator
-                    IntegrityLevel: bar bar
-                    md5: asdfasdfasdfasdfasdf
-                    sha256: asdfasdfasdfasdfasdfasdfasdfasdf
-                    ParentProcessId: 13
-                    ParentImage: valueB
-                    ParentCommandLine: Get-Path
+                    User: admin,
+                    CommandLine: cmdline /field=value,
+                    Image: cmd.exe,
+                    LogonId: logon_id_here,
+                    Product: Product Name,
+                    Company: Company Name,
+                    IntegrityLevel: High,
+                    CurrentDirectory: /current/working/directory,
+                    ProcessId: 1,
+                    ParentProcessId: 2,
+                    ParentCommandLine: cmdline2 /field1=value1,
+                    ParentImage: explorer.exe,
+                    ParentUser: guest,
+                    ParentIntegrityLevel: Low,
+                    ParentLogonId: logon_id_here_parent,
+                    ParentProduct: Parent Product Name,
+                    ParentCompany: Parent Company Name,
+                    md5: md5md5md5md5,
+                    sha256: sha256sha256sha256
                 condition: sel
         """)
-    ) == ['preset=xdr_process | filter (event_type = ENUM.PROCESS and event_sub_type = ENUM.PROCESS_START) and (action_process_os_pid = 12 and ' + 
-          'action_process_image_path = "valueA" and action_process_signature_product = "bar foo" and action_process_signature_vendor = "foo foo" and ' + 
-          'action_process_image_command_line = "invoke-mimikatz" and action_process_cwd = "/etc" and action_process_username = "administrator" and ' + 
-          'action_process_integrity_level = "bar bar" and action_process_image_md5 = "asdfasdfasdfasdfasdf" and ' + 
-          'action_process_image_sha256 = "asdfasdfasdfasdfasdfasdfasdfasdf" and actor_process_os_pid = 13 and actor_process_image_path = "valueB" and ' + 
-          'actor_process_image_command_line = "Get-Path")']
+    ) == ['preset=xdr_process | filter (event_type = ENUM.PROCESS and event_sub_type = ENUM.PROCESS_START) and (action_process_username = "admin," and action_process_image_command_line = "cmdline /field=value," and action_process_image_path = "cmd.exe," and action_process_logon_id = "logon_id_here," and action_process_signature_product = "Product Name," and action_process_signature_vendor = "Company Name," and action_process_integrity_level = "High," and action_process_cwd = "/current/working/directory," and action_process_os_pid = "1," and actor_process_os_pid = "2," and actor_process_image_command_line = "cmdline2 /field1=value1," and actor_process_image_path = "explorer.exe," and actor_effective_username = "guest," and actor_process_integrity_level = "Low," and actor_process_logon_id = "logon_id_here_parent," and actor_process_signature_product = "Parent Product Name," and actor_process_signature_vendor = "Parent Company Name," and action_process_image_md5 = "md5md5md5md5," and action_process_image_sha256 = "sha256sha256sha256")']
 
 def test_cortexxdr_file_mapping(cortexxdr_backend : CortexXDRBackend):
     assert cortexxdr_backend.convert(
@@ -125,17 +157,11 @@ def test_cortexxdr_file_mapping(cortexxdr_backend : CortexXDRBackend):
                 product: test_product
             detection:
                 sel:
-                    Image: valueA
-                    CommandLine: invoke-mimikatz
-                    ParentImage: valueB
-                    ParentCommandLine: Get-Path
                     TargetFilename: foo bar
                     SourceFilename: bar foo
                 condition: sel
         """)
-    ) == ['preset=xdr_file | filter event_type = ENUM.FILE and (actor_process_image_path = "valueA" and ' + 
-          'actor_process_image_command_line = "invoke-mimikatz" and causality_actor_process_image_path = "valueB" and ' + 
-          'causality_actor_process_command_line = "Get-Path" and action_file_name = "foo bar" and action_file_previous_file_name = "bar foo")']
+    ) == ['preset=xdr_file | filter event_type = ENUM.FILE and (action_file_name = "foo bar" and action_file_previous_file_name = "bar foo")']
 
 def test_cortexxdr_image_load_mapping(cortexxdr_backend : CortexXDRBackend):
     assert cortexxdr_backend.convert(
@@ -147,18 +173,12 @@ def test_cortexxdr_image_load_mapping(cortexxdr_backend : CortexXDRBackend):
                 product: test_product
             detection:
                 sel:
-                    Image: valueA
-                    CommandLine: invoke-mimikatz
-                    ParentImage: valueB
-                    ParentCommandLine: Get-Path
                     ImageLoaded: foo bar
                     md5: asdfasdfasdf
                     sha256: qwerqwerqwer
                 condition: sel
         """)
-    ) == ['dataset=xdr_data | filter event_type = ENUM.LOAD_IMAGE and (actor_process_image_path = "valueA" and ' + 
-          'actor_process_image_command_line = "invoke-mimikatz" and causality_actor_process_image_path = "valueB" and ' + 
-          'causality_actor_process_command_line = "Get-Path" and action_module_path = "foo bar" and action_module_md5 = "asdfasdfasdf" and ' + 
+    ) == ['preset=xdr_image_load | filter event_type = ENUM.LOAD_IMAGE and (action_module_path = "foo bar" and action_module_md5 = "asdfasdfasdf" and ' + 
           'action_module_sha256 = "qwerqwerqwer")']
 
 def test_cortexxdr_registry_mapping(cortexxdr_backend : CortexXDRBackend):
@@ -171,17 +191,11 @@ def test_cortexxdr_registry_mapping(cortexxdr_backend : CortexXDRBackend):
                 product: test_product
             detection:
                 sel:
-                    Image: valueA
-                    CommandLine: invoke-mimikatz
-                    ParentImage: valueB
-                    ParentCommandLine: Get-Path
                     TargetObject: foo bar
                     Details: bar foo
                 condition: sel
         """)
-    ) == ['preset=xdr_registry | filter event_type = ENUM.REGISTRY and (actor_process_image_path = "valueA" and ' + 
-          'actor_process_image_command_line = "invoke-mimikatz" and causality_actor_process_image_path = "valueB" and ' + 
-          'causality_actor_process_command_line = "Get-Path" and action_registry_key_name = "foo bar" and ' + 
+    ) == ['preset=xdr_registry | filter event_type = ENUM.REGISTRY and (action_registry_key_name = "foo bar" and ' + 
           '(action_registry_value_name = "bar foo" or action_registry_data = "bar foo"))']
 
 def test_cortexxdr_network_mapping(cortexxdr_backend : CortexXDRBackend):
@@ -194,13 +208,6 @@ def test_cortexxdr_network_mapping(cortexxdr_backend : CortexXDRBackend):
                 product: test_product
             detection:
                 sel:
-                    Image: valueA
-                    CommandLine: invoke-mimikatz
-                    ParentImage: valueB
-                    ParentCommandLine: Get-Path
-                    DestinationPort: 445
-                    DestinationIp: 0.0.0.0
-                    User: administrator
                     SourcePort: 135
                     SourceIp: 1.1.1.1
                     Protocol: udp
@@ -210,10 +217,8 @@ def test_cortexxdr_network_mapping(cortexxdr_backend : CortexXDRBackend):
                     src_port: 8080
                 condition: sel
         """)
-    ) == ['preset=network_story | filter event_type = ENUM.NETWORK and (actor_process_image_path = "valueA" and ' + 
-          'actor_process_image_command_line = "invoke-mimikatz" and causality_actor_process_image_path = "valueB" and ' + 
-          'causality_actor_process_command_line = "Get-Path" and (action_local_port = 445 or action_remote_port = 445) and ' +
-          '(action_local_ip = "0.0.0.0" or action_remote_ip = "0.0.0.0") and action_username = "administrator" and ' + 
+    ) == ['preset=network_story | filter event_type = ENUM.NETWORK and ((action_local_port = 445 or action_remote_port = 445) and ' +
+          '(action_local_ip = "0.0.0.0" or action_remote_ip = "0.0.0.0") and actor_effective_username = "administrator" and ' + 
           '(action_local_port = 135 or action_remote_port = 135) and (action_local_ip = "1.1.1.1" or action_remote_ip = "1.1.1.1") and ' + 
           'action_network_protocol = "udp" and (action_local_ip = "2.2.2.2" or action_remote_ip = "2.2.2.2") and ' + 
           '(action_local_port = 80 or action_remote_port = 80) and (action_local_ip = "3.3.3.3" or action_remote_ip = "3.3.3.3") and ' + 
